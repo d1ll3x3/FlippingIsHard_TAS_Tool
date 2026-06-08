a1a23e3e37f749788e3690bfed6486978f070bf9
Dani <dani@example.com>
2026-06-08 20:25:23 +0200

    Make build.bat portable: auto-detect game on any drive, fix names

--- build.bat
+++ build.bat
@@ -1,9 +1,30 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set GAME_PLUGINS=I:\SteamLibrary\steamapps\common\Flipping is Hard Demo\BepInEx\plugins\FlippingIsHardTrainer
+set DLL_NAME=FlippingIsHardTAS
+set DLL_PATH=bin\Debug\net6.0\%DLL_NAME%.dll
+set PLUGIN_FOLDER=FlippingIsHardTAS
 
-echo Building FlippingIsHardTrainer...
+echo Searching for game installation...
+
+set GAME_PATH=
+for %%D in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist "%%D:\SteamLibrary\steamapps\common\Flipping is Hard Demo" (
+        set GAME_PATH=%%D:\SteamLibrary\steamapps\common\Flipping is Hard Demo
+        goto found
+    )
+)
+
+echo Game not found in any SteamLibrary.
+pause
+exit /b 1
+
+:found
+echo Found: !GAME_PATH!
+
+set GAME_PLUGINS=!GAME_PATH!\BepInEx\plugins\%PLUGIN_FOLDER%
+
+echo Building %DLL_NAME%...
 dotnet build -c Debug
 
 if %ERRORLEVEL% NEQ 0 (
@@ -14,10 +35,10 @@ if %ERRORLEVEL% NEQ 0 (
 
 echo.
 echo Deploying to game...
-if not exist "%GAME_PLUGINS%" mkdir "%GAME_PLUGINS%"
-copy /Y "bin\Debug\net6.0\FlippingIsHardTrainer.dll" "%GAME_PLUGINS%\FlippingIsHardTrainer.dll"
+if not exist "!GAME_PLUGINS!" mkdir "!GAME_PLUGINS!"
+copy /Y "%DLL_PATH%" "!GAME_PLUGINS!\%DLL_NAME%.dll"
 
 echo.
 echo Done! Plugin deployed to:
-echo %GAME_PLUGINS%
+echo !GAME_PLUGINS!
 pause
